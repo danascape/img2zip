b9090305a827ba0497ebb437c175155d140355a7
iamsaalim <saalimquadri1@gmail.com>
2019-12-09 19:50:03 +0100

    tool: adding strip script

--- strip.sh
+++ strip.sh
@@ -1,1 +1,12 @@
-empty
+#!/usr/bin/env bash
+
+export ANYDEVICE_DIR=$PWD/tools/Anydevice
+
+if [[ -z "$(output/system.new.dat.br)" ]]; then
+    echo "Do you want to make any other compression zip"
+else 
+    echo "starting compress script"
+fi
+   mv output/system.new.dat.br $ANYDEVICE_DIR
+   cd $ANYDEVICE_DIR
+   make normal
