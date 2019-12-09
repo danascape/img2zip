927e6a07b2a1eaef2a2f0b848651c291e15ec583
danielle <saalimquadri1@gmail.com>
2019-12-09 13:06:26 +0530

    tool: move initial files to input dir.

--- img2brotli.sh
+++ img2brotli.sh
@@ -8,11 +8,14 @@ elif [[ -z "$(which forever)" ]]; then
     echo "Install brotli!"
     echo "sudo apt-get install brotli"
     
-elif [[ -z "$(find system.img)" ]]; then
+elif [[ -z "$(find input/system.img)" ]]; then
     echo "where is system image ?!"
     echo "paste system.img on current directory"
     
 else
     git fetch origin master
     git reset --hard origin/master
+    mkdir input 
+    mv system.img input/
 fi    
+
