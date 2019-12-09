a90595b9351c94d5abf773c4ab4a6cec7b182346
danielle <saalimquadri1@gmail.com>
2019-12-09 13:13:32 +0530

    tool: add basic commands in script

--- img2brotli.sh
+++ img2brotli.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export tools_dir=$PWD
+
 if [[ -z "$(which python)" ]]; then
     echo "Install python!"
     echo "sudo apt-get install python"
@@ -16,6 +18,20 @@ else
     git fetch origin master
     git reset --hard origin/master
     mkdir input 
+    mkdir output
     mv system.img input/
+    cd input/
+    mkdir compress
+    python $tools_dir/img2sdat.py system.img -o compress -v 4 -p system
+    cd compress 
+    brotli system.new.dat -2
+    mv system.new.dat.br ../../output
+    cd $tools_dir
 fi    
 
+if [[ -z "$(find output/system.new.dat.br)" ]]; then
+    echo "compress script failed check screen for error and rerun"
+    
+else
+    echo "img2brotli completed successfully."
+    echo "find brotli compressed image in output dir."
