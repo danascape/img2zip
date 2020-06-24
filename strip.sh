39b973c6d0de4bfa6d813252b6a00da1753f067e
iamsaalim <saalimquadri1@gmail.com>
2020-06-24 10:43:12 +0530

    Add a system image flasher * maybe will work for all
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>

--- strip.sh
+++ strip.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-export ANYDEVICE_DIR=$PWD/tools/Anydevice
+export ZIP_DIR=$PWD/tools/zip
 
 if [[ -z "$(output/system.new.dat.br)" ]]; then
     echo "Do you want to make any other compression zip"
 else
     echo "starting compress script"
 fi
-   mv "output/system.new.dat.br" "$ANYDEVICE_DIR"
-   cd "$ANYDEVICE_DIR"
+   mv "output/system.new.dat.br" "$ZIP_DIR"
+   cd "$ZIP_DIR"
    make normal
